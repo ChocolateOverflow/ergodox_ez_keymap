a6f130a1898678ca746f81d9a0564d25dcb6bb1e
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2023-04-28 21:55:21 +0700

    Added mouse layer tab combos; Added & applied alignment opts for clang-format

--- keymap.c
+++ keymap.c
@@ -1,54 +1,54 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#define STAB S(KC_TAB)
+#define STAB     S(KC_TAB)
 // home mods row
-#define MT_A LALT_T(KC_A)
-#define MT_S LGUI_T(KC_S)
-#define MT_D LCTL_T(KC_D)
-#define MT_F LSFT_T(KC_F)
-#define MT_J LSFT_T(KC_J)
-#define MT_K LCTL_T(KC_K)
-#define MT_L LGUI_T(KC_L)
-#define MT_SCLN LALT_T(KC_SCLN)
+#define MT_A     LALT_T(KC_A)
+#define MT_S     LGUI_T(KC_S)
+#define MT_D     LCTL_T(KC_D)
+#define MT_F     LSFT_T(KC_F)
+#define MT_J     LSFT_T(KC_J)
+#define MT_K     LCTL_T(KC_K)
+#define MT_L     LGUI_T(KC_L)
+#define MT_SCLN  LALT_T(KC_SCLN)
 // layer taps
-#define LT_Z LT(NUMPAD, KC_Z)
-#define LT_SLSH LT(MED, KC_SLSH)
+#define LT_Z     LT(NUMPAD, KC_Z)
+#define LT_SLSH  LT(MED, KC_SLSH)
 // thumb cluster
-#define NUM_ENT LT(NUMPAD, KC_ENT)
-#define NUM_SPC LT(NUMPLUS, KC_SPC)
-#define SYM_TAB LT(SYMBOL, KC_TAB)
+#define NUM_ENT  LT(NUMPAD, KC_ENT)
+#define NUM_SPC  LT(NUMPLUS, KC_SPC)
+#define SYM_TAB  LT(SYMBOL, KC_TAB)
 #define MED_BSPC LT(MED, KC_BSPC)
 // NUMPLUS layer home row mods
-#define MT_4 LALT_T(KC_4)
-#define MT_3 LGUI_T(KC_3)
-#define MT_2 LCTL_T(KC_2)
-#define MT_1 LSFT_T(KC_1)
-#define MT_0 LSFT_T(KC_0)
-#define MT_9 LCTL_T(KC_9)
-#define MT_8 LALT_T(KC_8)
-#define MT_7 LGUI_T(KC_7)
+#define MT_4     LALT_T(KC_4)
+#define MT_3     LGUI_T(KC_3)
+#define MT_2     LCTL_T(KC_2)
+#define MT_1     LSFT_T(KC_1)
+#define MT_0     LSFT_T(KC_0)
+#define MT_9     LCTL_T(KC_9)
+#define MT_8     LALT_T(KC_8)
+#define MT_7     LGUI_T(KC_7)
 // Layer access
-#define LT_NUM LT(NUMPAD, KC_F12)
-#define LT_MED LT(MED, KC_F12)
+#define LT_NUM   LT(NUMPAD, KC_F12)
+#define LT_MED   LT(MED, KC_F12)
 // mouse layer
-#define CT_ENT LCTL_T(KC_ENT)
-#define SH_SPC LSFT_T(KC_SPC)
-#define SH_TAB LSFT_T(KC_TAB)
-#define CT_DEL LCTL_T(KC_DEL)
-#define TD_SCR TD(TD_SCROLL_SCREENSHOT)
-#define TD_ENT TD(TD_ENTER_ESC)
+#define CT_ENT   LCTL_T(KC_ENT)
+#define SH_SPC   LSFT_T(KC_SPC)
+#define SH_TAB   LSFT_T(KC_TAB)
+#define CT_DEL   LCTL_T(KC_DEL)
+#define TD_SCR   TD(TD_SCROLL_SCREENSHOT)
+#define TD_ENT   TD(TD_ENTER_ESC)
 #define TD_BTN45 TD(TD_MOUSE_45)
 
 enum layers {
-    BASE = 0,
-    NUMPLUS,
-    SYMBOL,
-    NAV,
-    FN,
-    NUMPAD,
-    MED,
-    MOUSE,
+  BASE = 0,
+  NUMPLUS,
+  SYMBOL,
+  NAV,
+  FN,
+  NUMPAD,
+  MED,
+  MOUSE,
 };
 
 /***** KEYMAP *****/
@@ -152,178 +152,204 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+  rgb_matrix_sethsv_noeeprom(HSV_OFF);
 }
 
 bool rgb_matrix_indicators_user(void) {
-    if (keyboard_config.disable_layer_led) {
-        return false;
-    }
-    switch (biton32(layer_state)) {
-        case BASE:
-            rgb_matrix_set_color_all(RGB_RED);
-            break;
-        case NUMPLUS:
-            rgb_matrix_set_color_all(RGB_BLUE);
-            break;
-        case SYMBOL:
-            rgb_matrix_set_color_all(RGB_GREEN);
-            break;
-        case NAV:
-            rgb_matrix_set_color_all(RGB_ORANGE);
-            break;
-        case FN:
-            rgb_matrix_set_color_all(RGB_CYAN);
-            break;
-        case NUMPAD:
-            rgb_matrix_set_color_all(RGB_PINK);
-            break;
-        case MED:
-            rgb_matrix_set_color_all(RGB_PURPLE);
-            break;
-        case MOUSE:
-            rgb_matrix_set_color_all(RGB_MAGENTA);
-            break;
-        default:
-            if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(RGB_OFF);
-            break;
-    }
-    return true;
+  if (keyboard_config.disable_layer_led) {
+    return false;
+  }
+  switch (biton32(layer_state)) {
+  case BASE:
+    rgb_matrix_set_color_all(RGB_RED);
+    break;
+  case NUMPLUS:
+    rgb_matrix_set_color_all(RGB_BLUE);
+    break;
+  case SYMBOL:
+    rgb_matrix_set_color_all(RGB_GREEN);
+    break;
+  case NAV:
+    rgb_matrix_set_color_all(RGB_ORANGE);
+    break;
+  case FN:
+    rgb_matrix_set_color_all(RGB_CYAN);
+    break;
+  case NUMPAD:
+    rgb_matrix_set_color_all(RGB_PINK);
+    break;
+  case MED:
+    rgb_matrix_set_color_all(RGB_PURPLE);
+    break;
+  case MOUSE:
+    rgb_matrix_set_color_all(RGB_MAGENTA);
+    break;
+  default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(RGB_OFF);
+    break;
+  }
+  return true;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    layer_state_t layer = biton(state);
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-    switch (layer) {
-        // case BASE: no LED
-        case NUMPLUS:
-            ergodox_right_led_1_on();
-            break;
-        case SYMBOL:
-            ergodox_right_led_2_on();
-            break;
-        case NAV:
-            ergodox_right_led_3_on();
-            break;
-        case FN:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
-            break;
-        case NUMPAD:
-            ergodox_right_led_2_on();
-            ergodox_right_led_3_on();
-            break;
-        case MED:
-            ergodox_right_led_1_on();
-            ergodox_right_led_3_on();
-        case MOUSE:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
-            ergodox_right_led_3_on();
-            break;
-        default:
-            break;
-    }
-    return state;
+  layer_state_t layer = biton(state);
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+  switch (layer) {
+  // case BASE: no LED
+  case NUMPLUS:
+    ergodox_right_led_1_on();
+    break;
+  case SYMBOL:
+    ergodox_right_led_2_on();
+    break;
+  case NAV:
+    ergodox_right_led_3_on();
+    break;
+  case FN:
+    ergodox_right_led_1_on();
+    ergodox_right_led_2_on();
+    break;
+  case NUMPAD:
+    ergodox_right_led_2_on();
+    ergodox_right_led_3_on();
+    break;
+  case MED:
+    ergodox_right_led_1_on();
+    ergodox_right_led_3_on();
+  case MOUSE:
+    ergodox_right_led_1_on();
+    ergodox_right_led_2_on();
+    ergodox_right_led_3_on();
+    break;
+  default:
+    break;
+  }
+  return state;
 };
 
-/***** COMBOS *****/
+  /***** COMBOS *****/
 
-#define COMBO_COUNT 11
+#define COMBO_COUNT 15
 enum combo_events {
-    ESC_COMBO,           //
-    COPY_COMBO,          //
-    PASTE_COMBO,         //
-    TERM_COPY_COMBO,     //
-    TERM_PASTE_COMBO,    //
-    CUT_COMBO,           //
-    BTN4_COMBO,          //
-    BTN5_COMBO,          //
-    SCROLL_TOGGLE_COMBO, //
-    HOME_COMBO,          //
-    END_COMBO,           //
-    COMBO_LENGTH         //
+  // home row
+  ESC_COMBO,
+  COPY_COMBO,
+  PASTE_COMBO,
+  TERM_COPY_COMBO,
+  TERM_PASTE_COMBO,
+  // mouse top row
+  HOME_COMBO,
+  END_COMBO,
+  TAB_NEXT_COMBO,
+  TAB_PREV_COMBO,
+  // mouse home row
+  BTN4_COMBO,
+  BTN5_COMBO,
+  SCROLL_TOGGLE_COMBO,
+  // mouse bottom row
+  CUT_COMBO,
+  TAB_NEW_COMBO,
+  TAB_CLOSE_COMBO,
+  //
+  COMBO_LENGTH,
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
+// base layer
 const uint16_t PROGMEM esc_combo[]           = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM copy_combo[]          = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM paste_combo[]         = {KC_X, KC_V, COMBO_END};
 const uint16_t PROGMEM term_copy_combo[]     = {KC_C, KC_X, COMBO_END};
 const uint16_t PROGMEM term_paste_combo[]    = {KC_V, KC_X, COMBO_END};
-const uint16_t PROGMEM cut_combo[]           = {C(KC_C), C(KC_V), COMBO_END};
+// mouse top row
+const uint16_t PROGMEM home_combo[]          = {C(KC_A), C(KC_Y), COMBO_END};
+const uint16_t PROGMEM end_combo[]           = {C(KC_A), C(KC_Z), COMBO_END};
+const uint16_t PROGMEM tab_next_combo[]      = {KC_ESC, C(KC_Z), COMBO_END};
+const uint16_t PROGMEM tab_prev_combo[]      = {KC_ESC, C(KC_Y), COMBO_END};
+// mouse home row
 const uint16_t PROGMEM btn4_combo[]          = {KC_ESC, KC_BTN1, COMBO_END};
 const uint16_t PROGMEM btn5_combo[]          = {KC_ESC, KC_BTN2, COMBO_END};
 const uint16_t PROGMEM scroll_toggle_combo[] = {KC_ESC, KC_BTN3, COMBO_END};
-const uint16_t PROGMEM home_combo[]          = {C(KC_A), C(KC_Y), COMBO_END};
-const uint16_t PROGMEM end_combo[]           = {C(KC_A), C(KC_Z), COMBO_END};
+// mouse bottom row
+const uint16_t PROGMEM tab_new_combo[]       = {KC_ESC, C(KC_V), COMBO_END};
+const uint16_t PROGMEM tab_close_combo[]     = {KC_ESC, C(KC_C), COMBO_END};
+const uint16_t PROGMEM cut_combo[]           = {C(KC_C), C(KC_V), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [ESC_COMBO]           = COMBO(esc_combo, KC_ESC),            //
-    [COPY_COMBO]          = COMBO(copy_combo, C(KC_C)),          //
-    [PASTE_COMBO]         = COMBO(paste_combo, C(KC_V)),         //
-    [TERM_COPY_COMBO]     = COMBO(term_copy_combo, C(S(KC_C))),  //
-    [TERM_PASTE_COMBO]    = COMBO(term_paste_combo, C(S(KC_V))), //
-    [CUT_COMBO]           = COMBO(cut_combo, C(KC_X)),           //
-    [BTN4_COMBO]          = COMBO(btn4_combo, KC_BTN4),          //
-    [BTN5_COMBO]          = COMBO(btn5_combo, KC_BTN5),          //
-    [SCROLL_TOGGLE_COMBO] = COMBO_ACTION(scroll_toggle_combo),   //
-    [HOME_COMBO]          = COMBO(home_combo, KC_BTN5),          //
-    [END_COMBO]           = COMBO(end_combo, KC_BTN5),           //
+    // base layer
+    [ESC_COMBO]           = COMBO(esc_combo, KC_ESC),
+    [COPY_COMBO]          = COMBO(copy_combo, C(KC_C)),
+    [PASTE_COMBO]         = COMBO(paste_combo, C(KC_V)),
+    [TERM_COPY_COMBO]     = COMBO(term_copy_combo, C(S(KC_C))),
+    [TERM_PASTE_COMBO]    = COMBO(term_paste_combo, C(S(KC_V))),
+    // mouse top row
+    [HOME_COMBO]          = COMBO(home_combo, KC_BTN5),
+    [END_COMBO]           = COMBO(end_combo, KC_BTN5),
+    [TAB_NEXT_COMBO]      = COMBO(tab_next_combo, C(KC_TAB)),
+    [TAB_PREV_COMBO]      = COMBO(tab_prev_combo, C(STAB)),
+    // mouse home row
+    [BTN4_COMBO]          = COMBO(btn4_combo, KC_BTN4),
+    [BTN5_COMBO]          = COMBO(btn5_combo, KC_BTN5),
+    [SCROLL_TOGGLE_COMBO] = COMBO_ACTION(scroll_toggle_combo),
+    // mouse bottom row
+    [CUT_COMBO]           = COMBO(cut_combo, C(KC_X)),
+    [TAB_NEW_COMBO]       = COMBO(tab_new_combo, C(KC_X)),
+    [TAB_CLOSE_COMBO]     = COMBO(tab_close_combo, C(KC_X)),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
-    switch (combo_index) {
-        case SCROLL_TOGGLE_COMBO:
-            if (pressed) {
-                tap_code(KC_LNUM);
-                wait_ms(10);
-                tap_code(KC_LNUM);
-            }
-            break;
+  switch (combo_index) {
+  case SCROLL_TOGGLE_COMBO:
+    if (pressed) {
+      tap_code(KC_LNUM);
+      wait_ms(10);
+      tap_code(KC_LNUM);
     }
+    break;
+  }
 }
 
 /***** LEADER MACROS *****/
 
 void leader_end_user(void) {
-    if (leader_sequence_one_key(KC_B)) {
-        SEND_STRING("#!/bin/bash\n\n");
-    } else if (leader_sequence_one_key(KC_H)) {
-        SEND_STRING("python3 -m http.server\n");
-    } else if (leader_sequence_one_key(KC_L)) {
-        SEND_STRING("nc -lnvp ");
-    } else if (leader_sequence_one_key(KC_P)) {
-        SEND_STRING("#!/usr/bin/python3\n\n");
-    } else if (leader_sequence_two_keys(KC_D, KC_T)) {
-        SEND_STRING("../../../../../../etc/passwd");
-    } else if (leader_sequence_two_keys(KC_H, KC_P)) {
-        SEND_STRING("python3 -m http.server ");
-    } else if (leader_sequence_two_keys(KC_L, KC_H)) {
-        SEND_STRING("127.0.0.1");
-    } else if (leader_sequence_two_keys(KC_P, KC_S)) {
-        SEND_STRING("ps aux --forest\n");
-    } else if (leader_sequence_two_keys(KC_S, KC_S)) {
-        SEND_STRING("ss -lntp\n");
-    } else if (leader_sequence_two_keys(KC_X, KC_T)) {
-        SEND_STRING("export TERM=xterm\n");
-    } else if (leader_sequence_two_keys(KC_P, KC_B)) {
-        SEND_STRING("php://filter/convert.base64-encode/resource=");
-    } else if (leader_sequence_two_keys(KC_P, KC_T)) {
-        SEND_STRING("python3 -c \"import pty; pty.spawn('/bin/bash')\"\n");
-    } else if (leader_sequence_two_keys(KC_Z, KC_T)) {
-        SEND_STRING(SS_LCTL("z"));
-        SEND_STRING(SS_DELAY(100));
-        SEND_STRING("stty raw -echo; fg\n\n");
-        SEND_STRING(SS_DELAY(100));
-        SEND_STRING("export TERM=xterm\n");
-    } else if (leader_sequence_three_keys(KC_X, KC_S, KC_S)) {
-        SEND_STRING("<script>alert(window.origin)</script>");
-    } else if (leader_sequence_four_keys(KC_S, KC_U, KC_I, KC_D)) {
-        SEND_STRING("find / -perm -4000 2>/dev/null\n");
-    }
+  if (leader_sequence_one_key(KC_B)) {
+    SEND_STRING("#!/bin/bash\n\n");
+  } else if (leader_sequence_one_key(KC_H)) {
+    SEND_STRING("python3 -m http.server\n");
+  } else if (leader_sequence_one_key(KC_L)) {
+    SEND_STRING("nc -lnvp ");
+  } else if (leader_sequence_one_key(KC_P)) {
+    SEND_STRING("#!/usr/bin/python3\n\n");
+  } else if (leader_sequence_two_keys(KC_D, KC_T)) {
+    SEND_STRING("../../../../../../etc/passwd");
+  } else if (leader_sequence_two_keys(KC_H, KC_P)) {
+    SEND_STRING("python3 -m http.server ");
+  } else if (leader_sequence_two_keys(KC_L, KC_H)) {
+    SEND_STRING("127.0.0.1");
+  } else if (leader_sequence_two_keys(KC_P, KC_S)) {
+    SEND_STRING("ps aux --forest\n");
+  } else if (leader_sequence_two_keys(KC_S, KC_S)) {
+    SEND_STRING("ss -lntp\n");
+  } else if (leader_sequence_two_keys(KC_X, KC_T)) {
+    SEND_STRING("export TERM=xterm\n");
+  } else if (leader_sequence_two_keys(KC_P, KC_B)) {
+    SEND_STRING("php://filter/convert.base64-encode/resource=");
+  } else if (leader_sequence_two_keys(KC_P, KC_T)) {
+    SEND_STRING("python3 -c \"import pty; pty.spawn('/bin/bash')\"\n");
+  } else if (leader_sequence_two_keys(KC_Z, KC_T)) {
+    SEND_STRING(SS_LCTL("z"));
+    SEND_STRING(SS_DELAY(100));
+    SEND_STRING("stty raw -echo; fg\n\n");
+    SEND_STRING(SS_DELAY(100));
+    SEND_STRING("export TERM=xterm\n");
+  } else if (leader_sequence_three_keys(KC_X, KC_S, KC_S)) {
+    SEND_STRING("<script>alert(window.origin)</script>");
+  } else if (leader_sequence_four_keys(KC_S, KC_U, KC_I, KC_D)) {
+    SEND_STRING("find / -perm -4000 2>/dev/null\n");
+  }
 }
